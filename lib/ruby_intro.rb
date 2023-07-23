148769b2c27719c5eb814a933c5cc7399552d2af
Supakron-Ch <97943384+SupakronQ@users.noreply.github.com>
2023-07-24 01:46:54 +0700

    Commit Round 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,90 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+
+  sum = 0
+  arr.each { |x| sum = sum + x }
+  sum
+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  
+  if arr.length == 0 
+    return 0
+
+  elsif arr.length == 1
+    return arr[0]
+
+  else 
+    arr.sort! 
+    arr.reverse!
+    arr[0] + arr[1]
+  end
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  
+  return false if arr.length < 2 
+    
+  i,j = 0,1
+
+  while i < arr.length - 1 
+
+    return true  if arr[i] + arr[j] == n
+
+    if j == arr.length-1
+      i = i+1
+      j = i
+    end
+
+    j+=1
+
+  end
+  false
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  
+  "Hello, "+name
+
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  
+  return false if s == '' 
+  !(['a','e','i','o','u'].include?(s[0].downcase)) && /[a-z]/.match?(s[0].downcase)
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  
+    return false unless s.match?(/^[01]+$/)
+
+    s.to_i(2) % 4 == 0
+
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN number cannot be empty' if isbn.empty?
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', price)
+  end
 end
+
